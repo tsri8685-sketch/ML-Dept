ffc5cb8d4645b38a0d9afed61b9d02c43f4a6e2b
tsri8685-sketch <251573867+tsri8685-sketch@users.noreply.github.com>
2026-02-24 21:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+iidbaiaidmee
 window.addEventListener("load", function(){
   document.getElementById("loader").style.display = "none";
 });
@@ -140,3 +140,4 @@ function openSubPopup(event, id) {
         .forEach(p => p.classList.remove("active"));
     document.getElementById(id).classList.add("active");
 }
+
